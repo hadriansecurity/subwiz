72f8ab8486f82656585ed2a58f969b496fb5dd81
EduardoTerres <169451532+EduardoTerres@users.noreply.github.com>
2025-07-18 16:02:03 +0200

    standardize readme and terminal output before comparing

--- tests/test_readme_switches.py
+++ tests/test_readme_switches.py
@@ -1,6 +1,10 @@
-import os
 import subprocess
 
+def standardize_text(text):
+    text = text.replace(" ", "")
+    text = text.replace("\t", "")
+    text = text.replace("\n", "")
+    return text
 
 def test_():
     with open("README.md", "r") as f:
@@ -11,4 +15,10 @@ def test_():
     )
     help_text = result.stdout
 
+    # Sandardize texts
+    help_text = standardize_text(help_text)
+    readme = standardize_text(readme)
+
     assert help_text in readme
+
+test_()
