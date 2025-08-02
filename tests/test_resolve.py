21478dc12da33376bc236837660f43ca3356fab5
klaas <48212135+klaasmeinke@users.noreply.github.com>
2025-08-01 20:57:59 -0700

    update resolution concurrency

--- tests/test_resolve.py
+++ tests/test_resolve.py
@@ -3,5 +3,5 @@ from subwiz.main import run_resolution
 
 def test_():
     input_domains = {"api.hadrian.io", "random_test.hadrian.io", "app.hadrian.io"}
-    registered_domains = run_resolution(input_domains, resolution_lim=10)
+    registered_domains = run_resolution(input_domains, resolution_concurrency=10)
     assert registered_domains == {"api.hadrian.io", "app.hadrian.io"}
